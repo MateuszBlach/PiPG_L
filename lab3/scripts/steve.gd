be24cc25f697cfc4794ed110032bb856d7b7841a
MateuszBlach <mateusz.blach@interia.pl>
2024-05-30 17:53:28 +0200

    lab3 3/8

--- lab3/scripts/steve.gd
+++ lab3/scripts/steve.gd
@@ -1,28 +1,33 @@
 extends CharacterBody3D
 
 const SPEED = 6
+const ROTSPEED = 7
 
 func _ready():
 	pass
 
 func  _physics_process(delta):
 	
-	if Input.is_action_just_pressed("ui_right") and Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		velocity.x = 0
-	elif Input.is_action_just_pressed("ui_right"):
-		velocity.x = SPEED
-	elif Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_right"):
 		velocity.x = -SPEED
+		$MeshInstance3D.rotate_z(deg_to_rad(ROTSPEED))
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = SPEED
+		$MeshInstance3D.rotate_z(deg_to_rad(-ROTSPEED))
 	else:
-		velocity.x = lerp(velocity.x,0,0.1)
+		velocity.x = lerp(velocity.x,0.0,0.1)
 		
-	if Input.is_action_just_pressed("ui_up") and Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		velocity.z = 0
-	elif Input.is_action_just_pressed("ui_up"):
+	elif Input.is_action_pressed("ui_up"):
 		velocity.z = SPEED
-	elif Input.is_action_just_pressed("ui_down"):
+		$MeshInstance3D.rotate_x(deg_to_rad(-ROTSPEED))
+	elif Input.is_action_pressed("ui_down"):
 		velocity.z = -SPEED
+		$MeshInstance3D.rotate_x(deg_to_rad(ROTSPEED))
 	else:
-		velocity.z = lerp(velocity.z,0,0.1)
+		velocity.z = lerp(velocity.z,0.0,0.1)
 		
 	move_and_slide()
